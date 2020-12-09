2e652ff104ac68de4b5e3c17e88b21350867892c
androaap <38350296+androaap@users.noreply.github.com>
2020-12-09 17:10:20 +0700

    TugasModul2RSBK

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+    
   );
 }
 
+function navbar(){
+  return (
+    <div classname="nacbar">
+      <ul classname="avbar-nav"></ul>
+    </div>
+  )
+}
 export default App;
